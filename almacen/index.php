155b1bb7a46f371df095fcd0d275aa712af79a77
Styve-ops <smunera08@gmail.com>
2024-11-27 14:46:10 -0500

    se termino el primer modulo

--- almacen/index.php
+++ almacen/index.php
@@ -52,7 +52,6 @@ include ('../app/controllers/almacen/listado_de_productos.php');
                             <th><center>Nombre</center></th>
                             <th><center>Descripcion</center></th>
                             <th><center>Stock</center></th>
-                            
                             <th><center>Precio compra</center></th>
                             <th><center>Precio venta</center></th>
                             <th><center>Fecha compra</center></th>
@@ -75,7 +74,22 @@ include ('../app/controllers/almacen/listado_de_productos.php');
                                         </td>
                                         <td><?php echo $productos_dato['nombre']; ?></td>
                                         <td><?php echo $productos_dato['descripcion']; ?></td>
-                                        <td><?php echo $productos_dato['stock']; ?></td>
+                                          <?php
+                                           $stock_actual = $productos_dato['stock'];
+                                           $stock_maximo = $productos_dato['stock_maximo'];
+                                           $stock_minimo = $productos_dato['stock_minimo'];
+                                           if($stock_actual < $stock_minimo){ ?>
+                                                <td style="background-color: #ee868b"><center><?php echo $productos_dato['stock']; ?></center></td>
+                                            <?php
+                                           }else if ($stock_actual > $stock_maximo ) { ?>
+                                              <td style="background-color: #8ac68d"><center><?php echo $productos_dato['stock']; ?></center></td>
+
+                                            <?php
+                                           } else { ?>
+                                                    <td><center><?php echo $productos_dato['stock']; ?></center></td>
+                                            <?php
+                                           }
+                                          ?>
                                         
                                         <td><?php echo $productos_dato['precio_compra']; ?></td>
                                         <td><?php echo $productos_dato['precio_venta']; ?></td>
--- sistemadeventas.sql
+++ sistemadeventas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-11-2024 a las 17:12:55
+-- Tiempo de generación: 27-11-2024 a las 20:43:50
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -50,8 +50,8 @@ CREATE TABLE `tb_almacen` (
 --
 
 INSERT INTO `tb_almacen` (`id_producto`, `codigo`, `nombre`, `descripcion`, `stock`, `stock_minimo`, `stock_maximo`, `precio_compra`, `precio_venta`, `fecha_ingreso`, `imagen`, `id_usuario`, `id_categoria`, `fyh_creacion`, `fyh_actualizacion`) VALUES
-(3, 'P-00001', 'COCA QUINA', 'de 2 litro', 200, 20, 500, '9', 12, '2024-11-25', '2024-11-25-03-06-13__COCAQUINA.JPEG', 1, 3, '2024-11-25 15:06:13', '0000-00-00 00:00:00'),
-(4, 'P-00002', 'AUDIFONOS INALAMBRICOS', 'color negro', 200, 20, 150, '9', 15, '2024-11-25', '2024-11-25-10-19-14__audifonos.jpg', 1, 12, '2024-11-25 22:19:14', '0000-00-00 00:00:00');
+(3, 'P-00001', 'COCA QUINA', 'de 2 litro gaseosa', 200, 210, 500, '9', 12, '2024-11-25', '2024-11-25-03-06-13__COCAQUINA.JPEG', 1, 3, '2024-11-25 15:06:13', '2024-11-27 14:33:42'),
+(4, 'P-00002', 'AUDIFONOS INALAMBRICOS', 'color negro', 180, 20, 150, '9', 15, '2024-11-25', '2024-11-25-10-19-14__audifonos.jpg', 1, 12, '2024-11-25 22:19:14', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -77,7 +77,8 @@ INSERT INTO `tb_categorias` (`id_categoria`, `nombre_categoria`, `fyh_creacion`,
 (4, 'ELECTRODOMESTICOS', '2024-11-13 18:29:16', '2024-11-13 18:38:50'),
 (7, 'LIQUIDO', '2024-11-15 18:29:06', '0000-00-00 00:00:00'),
 (12, 'ELECTRONICOS', '2024-11-15 23:16:20', '0000-00-00 00:00:00'),
-(13, 's', '2024-11-16 17:30:03', '0000-00-00 00:00:00');
+(13, 'AMBIENTAL', '2024-11-16 17:30:03', '2024-11-27 14:31:41'),
+(14, 'FRAGANCIAS', '2024-11-27 14:32:16', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -89,7 +90,7 @@ CREATE TABLE `tb_compras` (
   `id_compra` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
   `nro_compra` int(11) NOT NULL,
-  `fecha_compra` datetime NOT NULL,
+  `fecha_compra` date NOT NULL,
   `id_proveedor` int(11) NOT NULL,
   `comprobante` varchar(255) NOT NULL,
   `id_usuario` int(11) NOT NULL,
@@ -104,9 +105,8 @@ CREATE TABLE `tb_compras` (
 --
 
 INSERT INTO `tb_compras` (`id_compra`, `id_producto`, `nro_compra`, `fecha_compra`, `id_proveedor`, `comprobante`, `id_usuario`, `precio_compra`, `cantidad`, `fyh_creacion`, `fyh_actualizacion`) VALUES
-(1, 3, 1, '2024-11-25 21:34:05', 3, 'FACTURA', 1, '200', 50, '2024-11-25 21:34:05', '2024-11-25 21:34:05'),
-(2, 4, 2, '2024-11-26 00:00:00', 8, 'FACTURA 1234', 1, '5600', 70, '2024-11-26 04:19:46', '0000-00-00 00:00:00'),
-(3, 4, 3, '2024-11-26 00:00:00', 3, 'FACTURA 1234', 1, '5600', 100, '2024-11-26 10:46:23', '0000-00-00 00:00:00');
+(1, 3, 1, '2024-11-25', 3, 'FACTURA', 1, '200', 50, '2024-11-25 21:34:05', '2024-11-25 21:34:05'),
+(3, 4, 0, '2024-11-26', 8, 'FACTURA 1234', 1, '5600', 150, '2024-11-26 10:46:23', '2024-11-27 11:53:15');
 
 -- --------------------------------------------------------
 
@@ -154,7 +154,8 @@ CREATE TABLE `tb_roles` (
 INSERT INTO `tb_roles` (`id_rol`, `rol`, `fyh_creacion`, `fyh_actualizacion`) VALUES
 (1, 'ADMINISTRADOR', '2024-11-12 15:38:06', '2024-11-14 07:22:29'),
 (2, 'CONTADOR', '2024-11-12 10:46:07', '0000-00-00 00:00:00'),
-(3, 'ALMACEN', '2024-11-13 10:12:38', '0000-00-00 00:00:00');
+(3, 'ALMACEN', '2024-11-13 10:12:38', '2024-11-27 14:31:09'),
+(4, 'PROGRAMADOR', '2024-11-27 14:31:02', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -178,7 +179,7 @@ CREATE TABLE `tb_usuarios` (
 --
 
 INSERT INTO `tb_usuarios` (`id_usuario`, `nombres`, `email`, `password_user`, `token`, `id_rol`, `fyh_creacion`, `fyh_actualizacion`) VALUES
-(1, 'Freddy Hilari', 'hilari@gmail.com', '$2y$10$DTj16OW4jgTs2kpmRl1qVO3S9qEFLPs9/BFOfq/PxqORPRzcGjcqW', '', 1, '2024-11-13 21:37:43', '2024-11-14 07:22:07');
+(1, 'Freddy Hilari', 'hilari@gmail.com', '$2y$10$QcmXOjCzjxIuckXg4s3mJeGS1YyZeGhhw0w2hMDUVLiURmDGqODnu', '', 1, '2024-11-13 21:37:43', '2024-11-27 14:29:15');
 
 --
 -- Índices para tablas volcadas
@@ -234,13 +235,13 @@ ALTER TABLE `tb_usuarios`
 -- AUTO_INCREMENT de la tabla `tb_almacen`
 --
 ALTER TABLE `tb_almacen`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_categorias`
 --
 ALTER TABLE `tb_categorias`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_compras`
@@ -252,19 +253,19 @@ ALTER TABLE `tb_compras`
 -- AUTO_INCREMENT de la tabla `tb_proveedores`
 --
 ALTER TABLE `tb_proveedores`
-  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_roles`
 --
 ALTER TABLE `tb_roles`
-  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_usuarios`
 --
 ALTER TABLE `tb_usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
